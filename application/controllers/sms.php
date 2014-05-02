626c87ab9c4ee24e97040aeb7741bde8525b68b8
TomKim2012 <tosh0948@gmail.com>
2014-05-02 08:13:52 +0300

    Initial works

--- application/controllers/sms.php
+++ application/controllers/sms.php
@@ -7,62 +7,101 @@ defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 require APPPATH . '/libraries/REST_Controller.php';
 require APPPATH . '/libraries/AfricasTalkingGateway.php';
 class Sms extends REST_Controller {
+	var $phoneNumber = "";
+	
 	function __construct() {
 		parent::__construct ();
 		date_default_timezone_set ( 'Africa/Nairobi' );
 		$this->load->library ( 'curl' );
 		$this->load->library ( 'CoreScripts' );
 		$this->load->model ( 'Member_Model', 'members' );
-		// $this->load->model ( 'Users_Model', 'users' );
 	}
 	function custSms_post() {
 		// 2.Read in the received values
-		$phoneNumber = $this->post ( "from" ); // sender's Phone Number
+		$phoneNumber = $this->post("from"); // sender's Phone Number
 		$shortCode = $this->post ( "to" ); // The short code that received the message
 		$text = $this->post ( "text" ); // Message text
 		$linkId = $this->post ( "linkId" ); // Used To bill the user for the response
 		$date = $this->post ( "date" ); // The time we received the message
 		$id = $this->post ( "id" ); // A unique id for this message
-		                            
-		// Add Balance from the text
 		
-		if ($phoneNumber) {
-			// 1. Use phoneNumber to get Client Code
-			if ($phoneNumber) {
-				$phoneNumber = "0" . substr ( $phoneNumber, 4 );
-				$custData = $this->members->getSingleMember ( 'Mobile', $phoneNumber );
-				
-				if ($custData ['MemberNo'] == "N/a") {
-					$message = "The phoneNumber you sent is not registered with the system. Kindly contact the Sacco for more details.";
-					$myresponse = $this->corescripts->_send_sms2 ( $phoneNumber, $message );
-					return;
-				}
-			}
+		$phoneNumber = "0" . substr ( $phoneNumber, 4 );
+		
+		// 1. If Text is Blank && phoneNumber exist
+		if ($text == "mridder") {
 			
-			$response = $this->corescripts->getTotals ( $custData ['MemberNo'] );
 			
-			// //---------------Compose the SMS-----------------------------------
-			$tDate = date ("d/m/Y");
-			$tTime = date("h:i A");
-			$message = "Dear " . $custData ['firstName'] . ", Your fare collections as at " . $tDate . " at " . $tTime
-					  .":";
-			$counter = 1;
-			foreach ( $response as $row ) {
-				$message .= "<". $counter ++ . ".".$row ['VehicleNo'] . " - Kshs " .number_format ( $row ['totals'] ).">";
-			}
+			$this->phoneNumber=$phoneNumber;
 			
-			//echo $message;
-			$sms_feedback = $this->corescripts->_send_sms2 ( $phoneNumber, $message );
-			if ($sms_feedback) {
-				echo "Success";
-			} else {
-				echo "Failed";
-			}
-			
+			$smsMessage = "Thanks for showing interest.Reply starting with INVESTOR," . "full names,IDNumber,".
+						   "gender and county separated by comma." . "Example: Investor,Joe Njeri,12345,Male,Muranga";
+			$this->sendToCustomer($smsMessage);
 		} else {
-			$message = 'Incorrect Format sent.Please add "Balance" to the Message text, then send again';
-			$this->corescripts->_send_sms2 ( $phoneNumber, $message );
+			$this->doRegistration ($text,$phoneNumber);
+		}
+	}
+	function validateFeedback($smsFeedback) {
+		if ($smsFeedback) {
+			echo "Success";
+		} else {
+			$message = "Muranga Sms Registration. This customer failed to receive SMS::".$this->phoneNumber;
+			$this->corescripts->_send_sms2 ( "254729472421", $message );
 		}
 	}
 	
+	function doRegistration($text,$phoneNumber) {
+		$this->phoneNumber=$phoneNumber;
+		
+		$custInput = explode ( ',', $text );
+		
+		if (sizeof($custInput) < 5) {
+			$smsMessage = "Incorrect Format: Reply starting with INVESTOR,full names,IDNumber,".
+						  "gender and county separated by comma." . "Example: Investor,Joe Njeri,12345,Male,Kiharo";
+			
+			$this->sendToCustomer($smsMessage);
+			return;
+		}
+		
+		// Ensure No Null Entry to Database
+		$errorMessage = "Registration Unsuccessful. You have not filled in:";
+		$hasError = false;
+		if ($custInput [1] == "") {
+			$errorMessage .= "Your FullNames,";
+			$hasError = true;
+		} elseif ($custInput [2] == "") {
+			$errorMessage .= "Your Id Number,";
+			$hasError = true;
+		} elseif ($custInput [3] == "") {
+			$errorMessage .= "Your Gender";
+			$hasError = true;
+		} elseif ($custInput [4] == "") {
+			$errorMessage .= "Your SubCounty:e.g.Kiharo";
+			$hasError = true;
+		}
+		
+		$cust = array (
+				'registrationCode' => $this->members->random_string ( 6 ),
+				'registrationDate' => date("Y-m-d H:i:s"),
+				'fullNames' => $custInput [1],
+				'idNumber' => $custInput [2],
+				'gender' => $custInput [3],
+				'subCounty' => $custInput [4] 
+		);
+		
+		if (!$hasError) {
+			print_r ( $cust );
+			$this->members->register($cust);
+			
+			$message = "You have successfully registered.Your Membership number is ".$cust['registrationCode'].
+						".Use paybill number 318150 to invest a minimum of KES 35 per day or KES 1000 per month.";
+			$this->sendToCustomer($message);
+			
+		} else {
+			$this->sendToCustomer($errorMessage);
+		}
+	}
+	function sendToCustomer($message) {
+		$smsFeedback=$this->corescripts->_send_sms2 ($this->phoneNumber, $message );
+		$this->validateFeedback($smsFeedback);
+	}
 }
--- application/models/member_model.php
+++ application/models/member_model.php
@@ -3,88 +3,31 @@ defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 class Member_Model extends CI_Model {
 	function __construct() {
 		parent::__construct ();
-		$this->db->query('Use naekana');
 	}
 	
-	/*
-	 * Repetition -Should find a solution to this immediately
-	 */
-	function getSingleMember($parameter, $value) {
-		
-		$this->db->where ( array (
-				$parameter => $value 
-		) );
-		$query = $this->db->get ( 'MembersDetails' );
-		
-		// print_r($query->result());
-		/* $fullNames = trim ( (isset ( $query->row ()->refno )) ? ($query->row ()->refno) : "N/a" ) . " " .
-				trim ( (isset ( $query->row ()->clname )) ? ($query->row ()->clname) : "N/a" ) . " " .
-				trim ( (isset ( $query->row ()->middlename )) ? ($query->row ()->middlename) : "N/a" ) . " " .
-				trim ( (isset ( $query->row ()->clsurname )) ? ($query->row ()->clsurname) : "N/a" );
-		 */
-			
-		$memberData = array (
-				'firstName' => trim ( (isset ( $query->row ()->Firstname )) ? ($query->row ()->Firstname) : "N/a" ),
-				'middleName' => trim ( (isset ( $query->row ()->Middlename )) ? ($query->row ()->Middlename) : "N/a" ),
-				'lastName' => trim ( (isset ( $query->row ()->Othernames )) ? ($query->row ()->Othernames) : "N/a" ),
-				'MemberNo' => trim ( (isset ( $query->row ()->MemberNo )) ? ($query->row ()->MemberNo) : "N/a" ) 
-		);
-		
-		return $memberData;
-	}
-	
-	function getVehicleNo_by_id($businessNo){
-		$this->db->query('Use naekana');
-		$this->db->select('VehicleNo');
-		$this->db->where ( array (
-				'businessNo' => $businessNo,
-				'Blocked'=>0
-		) );
-		$query = $this->db->get('MemberVehicleNo');
-
-		return $query->row()->VehicleNo;
-	}
-	
-	function getVehicles($memberNo){
-		$this->db->where ( array (
-				'memberNo' => $memberNo,
-				'Blocked'=>0
-		) );
-		$query = $this->db->get('MemberVehicleNo');
-		
-		//echo $this->db->last_query();
-		//print_r($query->result_array());
-		
-		$vehicleList= $query->result_array();
-		
-		$businessNos=array();
-		foreach ($vehicleList as $row) {
-			$data=array('VehicleNo' => $row['VehicleNo'],
-						'businessNo' => $row['businessNo'],
-			);
-			array_push($businessNos, $data);
+	function register($input){
+		$query=$this->db->insert('memberDetails', $input);
+		if($query){
+			return "OK|Thankyou, IPN has been successfully been saved.";
 		}
-		return $businessNos;
 	}
 	
-	function getTotals($businessNos){
-		$this->db->query('Use mobileBanking');
-		$response=array();
-		foreach ($businessNos as $row) {
-			$this->db->select_sum('mpesa_amt');
-			$this->db->where ( array (
-					'business_number' => $row['businessNo'],
-			) );
-			$query = $this->db->get('mTransportIPN');
-			
-			$amount= $query->row()->mpesa_amt;
-			
-			$data=array('VehicleNo' => $row['VehicleNo'],
-					'totals' => $amount,
-			);
-			array_push($response, $data);
+	function random_string($length = 4) {
+		$firstPart="M".substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),0,2);
+		
+		// Generate random 4 character string
+		$string = md5(microtime());
+		$secondPart = substr($string,1,$length);
+		$randomString = $firstPart.strtoupper($secondPart);
+	
+		//Confirm its not a duplicate
+		$this->db->where('registrationCode',$randomString);
+		$query=$this->db->get('memberDetails');
+		if($query->num_rows()>0){
+			random_string($length);
+		}else{
+			return $randomString;
 		}
-		return $response;
 	}
 	
 }
